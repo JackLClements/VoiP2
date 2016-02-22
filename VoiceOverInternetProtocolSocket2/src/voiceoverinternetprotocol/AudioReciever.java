6fe1461a2c3c4358f7becda9a8abad7ce2c60241
JackLClements <jack.clements@uea.ac.uk>
2016-02-22 17:49:57 +0000

    Changed layout of msg. Not sure it's helping much

--- VoiceOverInternetProtocolSocket2/src/voiceoverinternetprotocol/AudioReciever.java
+++ VoiceOverInternetProtocolSocket2/src/voiceoverinternetprotocol/AudioReciever.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Vector;
 import javax.sound.sampled.LineUnavailableException;
@@ -55,42 +56,74 @@ public class AudioReciever implements Runnable {
 		//***************************************************
 		//Main loop.
 		boolean running = true;
-		int burst = 0;
 		DatagramPacket[] orderedPackets = new DatagramPacket[256];
+		int burst = 0;
+		int lastWorkingPacket = 0;
 		while (running) {
 			try {
-				if (burst < 16) { //wrong, as this means 16 packets reciveved, does not compensate for packet loss
-					byte[] recieve = new byte[513];
-					DatagramPacket packet = new DatagramPacket(recieve, 0, 513);
-					
-					//Add data to packet
-					receiving_socket.receive(packet);
-					
-					//Unshuffle packet
-					byte header = recieve[0];
-					byte[] payload = new byte[512];
-					for (int i = 1; i < recieve.length; i++) {
-						payload[i - 1] = recieve[i];
-					}
-					DatagramPacket packet2 = new DatagramPacket(payload, payload.length);
-					System.out.println(header);
-					orderedPackets[header] = packet2;
-					burst++;
+
+				byte[] recieve = new byte[513];
+				DatagramPacket packet = new DatagramPacket(recieve, 0, 513);
+				//Add data to packet
+				receiving_socket.receive(packet);
+
+				//Unshuffle packet
+				byte header = recieve[0];
+				byte[] payload = new byte[512];
+				for (int i = 1; i < recieve.length; i++) {
+					payload[i - 1] = recieve[i];
+				}
+				DatagramPacket packet2 = new DatagramPacket(payload, payload.length);
+				
+				//Put packets in order via header
+				int orderedHeader;
+				int correctHeader = (int) header;
+				if (correctHeader < 0) {
+					orderedHeader = 256 + correctHeader;
 				} else {
-					for(int i = 0; i < 256 ; i++){ //265 IS WRONG, I NEEDS TO START AT THE PREVIOUS ENDPOINT, STORE THIS
+					orderedHeader = correctHeader;
+				}
+				orderedPackets[orderedHeader] = packet2;
+				if(burst >= 255){
+					for(int i = 0; i < 255; i++){
 						if(orderedPackets[i] == null){
-							//System.out.println("PACKET No. " + i);
+							player.playBlock(orderedPackets[lastWorkingPacket].getData());
 						}
 						else{
-							//System.out.println("PACKET No. " + i);
 							player.playBlock(orderedPackets[i].getData());
+							lastWorkingPacket = i;
 						}
+						
 					}
-					burst = 0;
+					//Arrays.fill(orderedPackets, null);
 				}
-
-				//player.playBlock(recieve);
-
+				burst++;
+				
+				//if (burst < 16) { //wrong, as this means 16 packets reciveved, does not compensate for packet loss
+				/*} else {
+				 for(int i = 16*x; i < (16*(x+1)); i++){ //265 IS WRONG, I NEEDS TO START AT THE PREVIOUS ENDPOINT, STORE THIS
+				 if(orderedPackets[i] == null){
+				 System.out.println("PACKET LOST No. " + i);
+				 }
+				 else{
+				 System.out.println("PACKET No. " + i);
+				 player.playBlock(orderedPackets[i].getData());
+				 }
+				 }
+				 System.out.println("NEW PACKET BURST");
+				 if(x < 15){
+				 System.out.println("X IS " + x);
+				 x++;
+				 }
+				 else{
+				 x = 0;
+				 System.out.println("RESET ARRAY");
+				 for(int i = 0; i < 256; i++){
+				 orderedPackets[i] = null;
+				 }
+				 }
+				 burst = 0;
+				 }*/
 			} catch (IOException e) {
 				System.out.println("ERROR: Audio Receiver: Some random IO error occured!");
 				e.printStackTrace();
